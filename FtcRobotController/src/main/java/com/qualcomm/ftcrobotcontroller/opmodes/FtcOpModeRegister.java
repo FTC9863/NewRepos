19b0336f4a6222944147a0cf8622a94f3fb99bdf
Mashellaneous <FTC9863@GMail.com>
2015-10-10 15:09:56 -0400

    added zk9

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -56,6 +56,7 @@ public class FtcOpModeRegister implements OpModeRegister {
      */
 
     manager.register("NullOp", NullOp.class);
+    manager.register("zK9TeleOp", zK9TeleOp.class);
 
     /*
      * Uncomment any of the following lines if you want to register an op mode.
